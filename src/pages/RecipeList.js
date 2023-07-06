52dc237159cd0fb0598a563def3a0730ccfb1143
saiderrahmani <didi.mousedidi@outlook.com>
2023-07-06 00:31:58 -0400

    update recipelist.js

---
+++ src/pages/RecipeList.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Card, Button } from 'react-bootstrap'; // Import Card and Button from React Bootstrap
+
+function RecipeList() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/recipes')
+      .then((response) => response.json())
+      .then((data) => setRecipes(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>All Recipes</h2>
+      <div className="recipe-list">
+        {recipes.map((recipe) => (
+          <Card key={recipe.id} className="mb-3">
+            <Card.Body>
+              <Card.Title>{recipe.title}</Card.Title>
+              <Card.Text>{recipe.description}</Card.Text>
+              <Button as={Link} to={`/recipes/${recipe.id}`} variant="primary">
+                View Details
+              </Button>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default RecipeList;
