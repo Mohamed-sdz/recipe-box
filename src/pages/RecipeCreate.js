5e801cb8c761c23089ea0d5f427178121a624c8a
saiderrahmani <didi.mousedidi@outlook.com>
2023-07-06 00:36:07 -0400

    update recipecreate.js

---
+++ src/pages/RecipeCreate.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+
+function RecipeCreate() {
+  const navigate = useNavigate();
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newRecipe = {
+      title,
+      description,
+    };
+
+    fetch('http://localhost:3000/recipes', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newRecipe),
+    })
+      .then((response) => response.json())
+      .then(() => {
+        setTitle('');
+        setDescription('');
+        navigate('/recipes');
+      });
+  };
+
+  return (
+    <div>
+      <h2>Create Recipe</h2>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="title">
+          <Form.Label>Title:</Form.Label>
+          <Form.Control
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="description">
+          <Form.Label>Description:</Form.Label>
+          <Form.Control
+            as="textarea"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Create
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default RecipeCreate;
+
