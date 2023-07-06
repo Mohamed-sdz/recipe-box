b7ea725289b4f4594e16a61aa293d0091382cf5d
saiderrahmani <didi.mousedidi@outlook.com>
2023-07-06 05:27:43 -0400

    update recipedetails

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { Card, Button } from 'react-bootstrap';
-
 function RecipeDetails() {
   const { id } = useParams();
   const [recipe, setRecipe] = useState(null);
