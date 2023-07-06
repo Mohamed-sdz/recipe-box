a0cc5e12d91fe9c8bf89cfb20957a52200ffa579
saiderrahmani <didi.mousedidi@outlook.com>
2023-07-06 00:34:05 -0400

    update profilepage.js

---
+++ src/pages/UserProfilePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserProfile from '../components/UserProfile';
+
+const UserProfilePage = () => {
+
+    return (    
+    <div>
+      <h2>User Profile</h2>
+      <UserProfile />    
+    </div>
+  );  
+};
+
+export default UserProfilePage;
