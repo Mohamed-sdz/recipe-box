f54e549ba13b3a31d35b7abd8bf4202a32988555
saiderrahmani <didi.mousedidi@outlook.com>
2023-07-06 00:15:54 -0400

    update navbar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,1 +1,28 @@
- 
+
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+const NavbarComponent = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <LinkContainer to="/recipes">
+        <Navbar.Brand>Recipe Box</Navbar.Brand>
+      </LinkContainer>
+      <Navbar.Toggle aria-controls="navbar-nav" />
+      <Navbar.Collapse id="navbar-nav">
+        <Nav className="ml-auto">
+          <LinkContainer to="/recipes">
+            <Nav.Link>Recipes</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/recipes/new">
+            <Nav.Link>Add Recipe</Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default NavbarComponent;
+
