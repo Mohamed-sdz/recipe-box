321280b0f7fc1b0b0eecec3f58b0cebe059c2f8f
saiderrahmani <didi.mousedidi@outlook.com>
2023-07-06 00:17:06 -0400

    update recipecard.js

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,1 +1,23 @@
- 
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+
+const RecipeCard = ({ title, ingredients, instructions }) => {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>
+          <strong>Ingredients:</strong> {ingredients}
+        </Card.Text>
+        <Card.Text>
+          <strong>Instructions:</strong> {instructions}
+        </Card.Text>
+        <Button variant="primary">Edit</Button>{' '}
+        <Button variant="danger">Delete</Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default RecipeCard;
+
